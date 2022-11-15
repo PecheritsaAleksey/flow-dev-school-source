3706125017983a1bf21bc9e8996e9031abe99607
PecheritsaAleksey <fromzeya@gmail.com>
2022-11-15 23:39:52 +0700

    Lesson 3-2

---
+++ module3/lesson3-2.js
@@ -0,0 +1,41 @@
+// console.log("HELLO!");
+
+//Рассказываю про переменные
+
+let greetings = "Hello!";
+console.log(greetings);
+
+// Создаём и пытаемся изменить, смотрим ошибку
+// const constGreetings = "Helloo!";
+
+// constGreetings = "Hello!!!";
+
+// console.log(constGreetings);
+
+
+//Регистрозависимый
+let Greetings = "Hey!";
+
+console.log(Greetings);
+
+/*
+
+Перенос строк, показать примеры 
+1 
++
+2
+
+*/
+
+let a = 1 + 4;
+console.log(a);
+
+//Комментарий строки
+
+/*
+
+КОММЕНТАРИЙ
+ДЛЯ НЕСКОЛЬКИХ
+СТРОК
+
+ */
