b7fb545697fc4bdbe658441f2d83f98cb154ac0b
PecheritsaAleksey <fromzeya@gmail.com>
2022-11-17 12:31:11 +0700

    Add lesson 3-4

---
+++ module3/lesson3-4.js
@@ -0,0 +1,25 @@
+console.log("1 + 2 =", 1 + 2);
+console.log("3 - 5 =", 3 - 5);
+console.log("3 * 5 =", 3 * 5);
+console.log(("3 / 7 =", 3 / 7).toFixed(2));
+
+console.log("2 ** 8 =", 2 ** 8);
+
+console.log("19 % 5 =", 19 % 5);
+
+//Таблица приоритетов на MDN
+
+console.log("one" + "two");
+console.log("5" - 3);
+
+let newVar = 10;
+newVar++;
+newVar *= 2;
+console.log(newVar);
+
+let falseValue = false;
+let trueValue = true;
+
+console.log(trueValue && falseValue);
+console.log(trueValue || falseValue);
+console.log(!trueValue || falseValue);
