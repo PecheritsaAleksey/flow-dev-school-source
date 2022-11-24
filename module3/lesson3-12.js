9df8ef5b2d443825777417249755dc1283296bdd
PecheritsaAleksey <fromzeya@gmail.com>
2022-11-24 20:22:35 +0700

    Add lesson 3-12

---
+++ module3/lesson3-12.js
@@ -0,0 +1,53 @@
+// if
+
+let a = 5;
+
+if (a > 3) {
+  console.log("a > 3");
+}
+
+//Преведение к логическому типу
+
+// console.log(Boolean([].length));
+console.log(Boolean([]));
+console.log(Boolean("WOW!"));
+console.log(Boolean(""));
+
+let userName = "Alex";
+
+if (!userName) {
+  console.log("Имя необходимо!");
+}
+
+let b = 8;
+
+// if (b > 10) {
+//   console.log("b > 10");
+// } else if (b > 9) {
+//   console.log("b > 9");
+// }
+// else if () {
+
+// }
+
+b > 10 ? console.log("b > 10") : b > 9 ? console.log("b > 9") : console.log("b < 9");
+
+// let userRole = "Alex";
+let userRole = "admin";
+
+// if (userRole === "admin") {
+// } else if (userRole === "superadmin") {
+// } else if (userRole === "user") {
+// }
+
+switch (userRole) {
+  case "admin":
+    console.log("Hello admin!");
+    break;
+  case "superadmin":
+    console.log("Hello superadmin!");
+    break;
+  default:
+    console.log("Who are you?");
+    break;
+}
